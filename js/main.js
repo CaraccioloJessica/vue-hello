ae46b32aaded2233818babda25da7e9af1b3e41e
Jessica <jesscara.993@gmail.com>
2022-11-25 14:52:15 +0100

    basic hello world done

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello World!'
+      }
+    }
+  }).mount('#title')
